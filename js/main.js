dde313215a8e7706709e6ad9f04dbf01c07676ec
Manuel Strebel <manuel_st@msn.com>
2018-02-04 12:52:31 +0000

    Apply fixes from StyleCI

--- js/main.js
+++ js/main.js
@@ -2,40 +2,42 @@
 var timeout;
 
 // Listen for resize events
-window.addEventListener('resize', function (event) {
-	console.log('no debounce');
+window.addEventListener(
+    'resize',
+    function(event) {
+        console.log('no debounce');
 
-	// If timer is null, reset it to 66ms and run your functions.
-	// Otherwise, wait until timer is cleared
-	if (!timeout) {
-		timeout = setTimeout(function () {
+        // If timer is null, reset it to 66ms and run your functions.
+        // Otherwise, wait until timer is cleared
+        if (!timeout) {
+            timeout = setTimeout(function() {
+                // Reset timeout
+                timeout = null;
 
-			// Reset timeout
-			timeout = null;
-
-			// Run our resize functions
-			console.log('debounced');
-
-		}, 66);
-	}
-}, false);
+                // Run our resize functions
+                console.log('debounced');
+            }, 66);
+        }
+    },
+    false
+);
 
 //window.addEventListener('resize', resizeText)
-document.querySelector('#fitme').addEventListener('keyup', resizeText)
+document.querySelector('#fitme').addEventListener('keyup', resizeText);
 
 function resizeText({ target }) {
-	console.count('qwe')
-	let height = document.documentElement.clientHeight;
-	let width = document.documentElement.clientWidth;
-	let fontSize = height;
+    console.count('qwe');
+    let height = document.documentElement.clientHeight;
+    let width = document.documentElement.clientWidth;
+    let fontSize = height;
 
-	[100, 50, 10, 2, 1].forEach(function (step) {
-		for (; fontSize > 0; fontSize -= step) {
-			target.style.fontSize = fontSize + 'px';
-			if (target.scrollWidth <= width && target.offsetHeight <= height) {
-				break;
-			}
-		}
-		fontSize += step;
-	});
+    [100, 50, 10, 2, 1].forEach(function(step) {
+        for (; fontSize > 0; fontSize -= step) {
+            target.style.fontSize = fontSize + 'px';
+            if (target.scrollWidth <= width && target.offsetHeight <= height) {
+                break;
+            }
+        }
+        fontSize += step;
+    });
 }
