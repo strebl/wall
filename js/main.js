98578912b17363222c046ca3d56b3958a3404e08
Manuel Strebel <manuel.strebel@gibb.ch>
2018-02-04 13:48:06 +0100

    remove jquery

--- js/main.js
+++ js/main.js
@@ -1,25 +1,41 @@
-var fitme = function() {
-	$('#fitme').textfill({
-		//debug: true,
-		maxFontPixels   : 9999,
-		minFontPixels   : 1//,
-		//innerTag: 'textarea',
-	    /*success: function(el) {
-	        console.log("yay!")
-	    },
-	    fail: function() {
-	        alert("boo hoo!")
-	    }*/
+// Setup a timer
+var timeout;
+
+// Listen for resize events
+window.addEventListener('resize', function (event) {
+	console.log('no debounce');
+
+	// If timer is null, reset it to 66ms and run your functions.
+	// Otherwise, wait until timer is cleared
+	if (!timeout) {
+		timeout = setTimeout(function () {
+
+			// Reset timeout
+			timeout = null;
+
+			// Run our resize functions
+			console.log('debounced');
+
+		}, 66);
+	}
+}, false);
+
+//window.addEventListener('resize', resizeText)
+document.querySelector('#fitme').addEventListener('keyup', resizeText)
+
+function resizeText({ target }) {
+	console.count('qwe')
+	let height = document.documentElement.clientHeight;
+	let width = document.documentElement.clientWidth;
+	let fontSize = height;
+
+	[100, 50, 10, 2, 1].forEach(function (step) {
+		for (; fontSize > 0; fontSize -= step) {
+			target.style.fontSize = fontSize + 'px';
+			if (target.scrollWidth <= width && target.offsetHeight <= height) {
+				break;
+			}
+		}
+		fontSize += step;
 	});
 }
-
-$('#fitme').on('keyup', function() {
-	$('#fitme span').prop('contentEditable', 'false');
-
-	fitme();
-
-	$('#fitme span').prop('contentEditable', 'true');
-})
-
-fitme();
-$('#fitme span').prop('contentEditable', 'true').focus();
